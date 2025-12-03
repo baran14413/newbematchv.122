28b37a62771c8f1e5243a00c0d9ab3fd7d4e87f4
Wilmar Nickel <baranguler.65091@gmail.com>
2025-12-03 17:58:04 +0000

    Try fixing this error: `Console Error: Encountered two children with the

--- src/app/(main)/discover/page.tsx
+++ src/app/(main)/discover/page.tsx
@@ -75,7 +75,7 @@ export default function DiscoverPage() {
                 
                 return (
                   <motion.div
-                    key={profile.id}
+                    key={`${profile.id}-${index}`}
                     drag={isTop}
                     dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
                     onDragEnd={(e, info) => {
@@ -250,5 +250,3 @@ export default function DiscoverPage() {
     </div>
   );
 }
-
-    
