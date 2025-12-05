8c318dd1198548fa0f057e2e91ee5e4ca545e2c4
Wilmar Nickel <baranguler.65091@gmail.com>
2025-12-05 14:40:34 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7901925384-66839",
+  "appId": "1:589415008748:web:03d9405e4d3f67f0b918c9",
+  "apiKey": "AIzaSyBfATsVBUg8ewSIfXJ6J27JuYU1tQgChYk",
+  "authDomain": "studio-7901925384-66839.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "589415008748"
+};
